85c1e96e0281f3feb23038795c2e61c630d4044c
BrittanyThirty <bmkthirty@gmail.com>
2018-01-11 20:52:29 -0800

    tests rebase

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul").prepend("<li>Hello!</li>");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul").prepend("<li>Goodbye!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul").prepend("<li>Stop copying me!</li>");
+  });
+
+  $(button#hello).click(function() {
+    $("ul#user).prepemd("<li>Hello</li>")"
+  })
+});
+$("ul#user").children("li").first().click(function() {
+  $(this).remove();
+});
+$("ul#webpage").children("li").first().click(function() {
+  $(this).remove();
+});
